0aae89e0f48e199db9edaf5d9ba273ff28a05cdf
Joan Mas <joan.mas@esblada.com>
2022-08-03 11:54:43 +0200

    real use case example

---
+++ src/Application/Calculator.ts
@@ -0,0 +1,9 @@
+export default class Calculator {
+    public rest(a: number, b: number): number {
+        return a - b;
+    }
+    public sum(a: number, b: number): number {
+        //Extra complex function
+        return a + b;
+    }
+}
--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,13 +1,13 @@
-describe("Example test", () => {
+import Calculator from "../Application/Calculator";
 
+describe("Example test", () => {
     it("It sum well 👍", async () => {
-        
         const A = 3;
         const B = 2;
 
-        const result = A + B;
-    
+        const useCase = new Calculator();
+        const result = useCase.sum(A, B);
+
         expect(result).toBe(5);
-    })
-    
-})
+    });
+});
