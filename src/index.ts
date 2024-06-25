94916c3474b3956e94e01e73407252f9c9a4c600
Joan Mas <joan.mas@esblada.com>
2024-06-25 13:25:00 +0200

    handle unhandled exceptions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import bodyParser from "body-parser";
 const app = express();
 import routes from "./routes";
+
+process.on("uncaughtException", (err) => {
+    console.error(err);
+});
 
 const port: number = parseInt(process.env.PORT || "3000");
 app.use(bodyParser.urlencoded({ extended: false }));
